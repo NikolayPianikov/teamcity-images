9d889303dc28f29c9e85abe0f4d2d09d0768bf8b
admin <admin@TeamCity>
2020-02-18 14:53:34 +0300

    TeamCity change in 'docker / docker images' project: runners of 'build image 2' build configuration were updated

--- .teamcity/patches/buildTypes/BuildImage.kts
+++ .teamcity/patches/buildTypes/BuildImage.kts
@@ -22,17 +22,18 @@ changeBuildType(RelativeId("BuildImage")) {
             dockerCommand {
                 name = "step 1"
                 commandType = build {
-                    source = content {
-                        content = """
-                            aaaa
-                            cdscdsc
-                            $TQ
-                            3456t546i-01938097&*^&*${'$'}&#%${'$'}^&#
-                        """.trimIndent()
+                    source = file {
+                        path = """generated\linux\Agent\Ubuntu\18.04\Dockerfile"""
                     }
                     commandArgs = "--pull"
                 }
                 param("dockerImage.platform", "windows")
+                param("dockerfile.content", """
+                    aaaa
+                    cdscdsc
+                    $TQ
+                    3456t546i-01938097&*^&*${'$'}&#%${'$'}^&#
+                """.trimIndent())
             }
         }
         insert(1) {
